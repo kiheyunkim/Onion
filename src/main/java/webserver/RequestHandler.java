e99b1b08e7f4a07a6f601802ef64628b934f1f3f
kiheyunkim <kiheyunKim@gmail.com>
2021-01-01 23:26:24 +0900

    add 1 step

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,14 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,16 +23,34 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        ) {
+            String line;
+            while((line= bufferedReader.readLine()) != null && line.length() > 0){
+                log.info(line);
+            }
 
-            bufferedReader.lines().forEach(System.out::println);
+            /*
+
+            int count = 0;
+            bufferedReader.lines().forEach(line -> {
+                log.info(line);
+                log.info(String.valueOf(count));
+            });
+            */
+
+            //bufferedReader.close();
+            //inputStreamReader.close();
+            //List<String> collect = bufferedReader.lines().filter(Objects::nonNull).filter(line -> !line.isEmpty()).collect(Collectors.toList());
+            //List<String> collect = bufferedReader.lines().collect(Collectors.toList());
 
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
+            e.printStackTrace();
             log.error(e.getMessage());
         }
     }
